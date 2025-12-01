5e59763061054abbb87f20b832abe49d06715070
Abdo <a.alsheykh@a.alsheykh>
2025-12-01 14:09:41 +0300

    feat: add missing annotation validator

--- plugins/entity-scaffolder/src/components/EntityScaffolderContent/EntityScaffolderContent.tsx
+++ plugins/entity-scaffolder/src/components/EntityScaffolderContent/EntityScaffolderContent.tsx
@@ -3,7 +3,10 @@
  * Licensed under the terms of the Apache-2.0 license. See LICENSE file in project root for terms.
  */
 import { Entity } from '@backstage/catalog-model';
-import { useEntity } from '@backstage/plugin-catalog-react';
+import { 
+  MissingAnnotationEmptyState,
+  useEntity 
+} from '@backstage/plugin-catalog-react';
 import { TemplateEntityV1beta3 } from '@backstage/plugin-scaffolder-common';
 import {
   SecretsContextProvider,
@@ -11,6 +14,8 @@ import {
 import type { JsonValue } from '@backstage/types';
 import { EmbeddedScaffolderWorkflow } from '@frontside/backstage-plugin-scaffolder-workflow';
 import React from 'react';
+
+import { ENTITY_SCAFFOLDER_ANNOTATION } from '../../annotations';
 
 /**
  * @public
@@ -39,6 +44,10 @@ export const EntityScaffolderContent = ({
   ScaffolderFieldExtensions,
 }: EntityScaffolderContentProps) => {
   const { entity } = useEntity();
+
+  const entityScaffolderAnnotationValue =
+    entity.metadata.annotations?.[ENTITY_SCAFFOLDER_ANNOTATION];
+  
   const templateEntity = {
     apiVersion: 'scaffolder.backstage.io/v1beta3',
     kind: 'Template',
@@ -48,18 +57,27 @@ export const EntityScaffolderContent = ({
     },
     spec: {},
   } as TemplateEntityV1beta3;
+
+  if (
+    entityScaffolderAnnotationValue 
+  ) {
+    return (
+      <SecretsContextProvider>
+        <EmbeddedScaffolderWorkflow
+          namespace={templateNamespace}
+          templateName={templateName}
+          initialState={buildInitialState(entity, templateEntity)}
+          onError={(error: Error | undefined) => (
+            <h2>{error?.message ?? 'Error running workflow'}</h2>
+          )}
+        >
+          {ScaffolderFieldExtensions ?? null}
+        </EmbeddedScaffolderWorkflow>
+      </SecretsContextProvider>
+    );
+  }
   return (
-    <SecretsContextProvider>
-      <EmbeddedScaffolderWorkflow
-        namespace={templateNamespace}
-        templateName={templateName}
-        initialState={buildInitialState(entity, templateEntity)}
-        onError={(error: Error | undefined) => (
-          <h2>{error?.message ?? 'Error running workflow'}</h2>
-        )}
-      >
-        {ScaffolderFieldExtensions ?? null}
-      </EmbeddedScaffolderWorkflow>
-    </SecretsContextProvider>
+    <MissingAnnotationEmptyState annotation={ENTITY_SCAFFOLDER_ANNOTATION} />
+      
   );
 };
